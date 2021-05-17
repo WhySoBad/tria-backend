06f70f450b009c9041ed3fa8ede8093474cf6b1b
Corsin <hermcors@gmail.com>
2021-05-17 19:38:40 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,7 @@ async function bootstrap(): Promise<void> {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useStaticAssets(join(__dirname, '..', 'static'));
   app.enableCors({
-    origin: '*',
-    methods: 'OPTIONS,GET,PUT,POST,DELETE',
+    origin: ['http://localhost:*', 'http://127.0.0.1:*'],
     credentials: true,
   });
   app.listen(3000);
